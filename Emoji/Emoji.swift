eeff3a403bc363742c52dc4cb0eb57cab96b370b
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-01-08 16:08:27 +0300

    Emoji Array added

---
+++ Emoji/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  Emoji
+//
+//  Created by Ayça Işık on 8.01.2023.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+
